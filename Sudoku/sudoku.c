fa652daf6b641313679b0c2cc62758f9f5c6550d
danielribeiro0 <184285175+danielribeiro0@users.noreply.github.com>
2024-10-29 14:47:49 -0300

    Sudoku interativo #1

--- Sudoku/sudoku.c
+++ Sudoku/sudoku.c
@@ -1,3 +1,37 @@
-  int main(){
-    
+#include <stdio.h>
+
+void preencher_sudoku (int M[3][3][3][3]) {
+  int i, j, k, l;
+  for (i = 0; i < 3; i++) {
+    for (k = 0; k < 3; k++) {
+      for (j = 0; j < 3; j++) {
+        for (l = 0; l < 3; l++) {
+          scanf("%d", &M[i][j][k][l]);
+        }
+      }
+    }
   }
+}
+
+void print_sudoku(int matriz[3][3][3][3]) {
+  int i, j, k, l;
+  for(i=0; i<3; i++) {
+    for(k=0; k<3; k++) {
+      for(j=0; j<3; j++) {
+        for(l=0; l<3; l++) {
+          printf("[%d] ", matriz[i][j][k][l]);
+        }
+        if(j<2) printf("| ");
+      }
+      printf("\n");
+    }
+    if(i<2) printf("---------------------------------------\n");
+  }
+}
+
+int main() {
+  int matriz[3][3][3][3];
+
+  preencher_sudoku(matriz);
+  print_sudoku(matriz);
+}
