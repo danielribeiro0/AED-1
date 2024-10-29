714fe009e29203f72dea9e1a169dbb00b23453f6
danielribeiro0 <184285175+danielribeiro0@users.noreply.github.com>
2024-10-29 15:15:52 -0300

    Sudoku interativo #2

--- Sudoku/sudoku.c
+++ Sudoku/sudoku.c
@@ -29,9 +29,30 @@ void print_sudoku(int matriz[3][3][3][3]) {
   }
 }
 
+void alterar_sudoku(int linha, int coluna, int slinha, int scoluna, int matriz[3][3][3][3]) {
+  int n;
+  printf("Digite o valor para alterar: ");
+  scanf("%d", &n);
+  matriz[linha][coluna][slinha][scoluna] = n;
+}
+
 int main() {
   int matriz[3][3][3][3];
 
   preencher_sudoku(matriz);
   print_sudoku(matriz);
+
+  int linha, coluna, slinha, scoluna;
+
+  int x;
+  printf("Deseja alterar a matriz? 1(sim) -1(não)\n");
+  scanf("%d", &x);
+  while(x != -1) {
+    printf("Passe a coordenada do item que deseja alterar (0, 1, 1, 2):");
+    scanf("%d %d %d %d", &linha, &coluna, &slinha, &scoluna);
+    alterar_sudoku(linha, coluna, slinha, scoluna, matriz);
+    print_sudoku(matriz);
+    printf("Deseja continuar alterarando a matriz? 1(sim) -1(não)\n");
+    scanf("%d", &x);
+  };
 }
